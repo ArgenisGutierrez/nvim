644b4a4b0cd7d0687d6bb4b277ba813850ac29d3
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2024-02-14 19:23:59 -0600

    ToggleTerm
    Toggleterm y keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,13 @@ keymap.set("n", "|", ":vsplit<Return>", opts)
 -- Buffer
 keymap.set("n", "<S-l>", ":tabnext<cr>")
 keymap.set("n", "<S-h>", ":tabprevious<cr>")
+
+--Terminal
+keymap.set({"n","t"}, "<A-1>", "<ESC><cmd>1ToggleTerm direction=vertical name=vertical<cr>")
+keymap.set({"n","t"}, "<A-2>", "<ESC><cmd>2ToggleTerm direction=horizontal name=horizontal<cr>")
+keymap.set({"n","t"}, "<A-3>", "<ESC><cmd>3ToggleTerm direction=float name=float<cr>")
+keymap.set("t", "<ESC>", [[<C-\><C-n>]], opts)
+keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
+keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
+keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
+keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
