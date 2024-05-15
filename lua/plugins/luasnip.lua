72e22ed509359a27eca4642549cd6efb90ce85ce
ArgenisGutierrez <argenis.v.gtz@gmail.com>
2024-05-15 11:14:07 -0600

    Reboot Configs
    Se restablecio las configuracionese de neovim a para empezar desde 0
    estudiando lua.

--- lua/plugins/luasnip.lua
+++
@@ -1,8 +0,0 @@
---NOTE: Plugin expandir y personalidar snipets dinamicos
---https://github.com/L3MON4D3/LuaSnip
-return{
-  "L3MON4D3/LuaSnip",
-  version = "v2.*",
-  main = "config.plugins.luasnip",
-  config = true,
-}
--- lua/plugins/move.lua
+++
@@ -1,7 +0,0 @@
---NOTE: Plugin para mover lineas de codigo con Alt
---https://github.com/matze/vim-move
-return {
-  'matze/vim-move',
-  config = function()
-  end
-}
