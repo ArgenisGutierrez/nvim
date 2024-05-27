9ea0be9aeff7679cfbc722f391857d9041aec57c
ArgenisGutierrez <argenis.v.gtz@gmail.com>
2024-05-27 12:50:04 -0600

    Configuracion Base
    Se configuro solo lazy, el colorcheme y el plugin transparent.

---
+++ lua/config/settings.lua
@@ -0,0 +1,10 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.g.mapleader = " "
+vim.g.transparent_enabled = true
+
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
---
+++ lua/plugins/ayu.lua
@@ -0,0 +1,15 @@
+-- Colorscheme https://github.com/Shatur/neovim-ayu
+return {
+	'Shatur/neovim-ayu',
+	name = "ayu",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require('ayu').setup({
+      overrides ={
+        LineNr = {fg = '#3fa6ff'}
+      }
+		})
+	vim.cmd([[colorscheme ayu-dark]])
+	end
+}
