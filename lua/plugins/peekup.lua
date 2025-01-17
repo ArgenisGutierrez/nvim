766a5a6e26ea832908a98fe331628626022220a1
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-01-17 14:00:45 -0600

    Fix(peekup):Remove pluging
    se elimino el pluging ya que telescope puede hacerlo tambien.

--- lua/plugins/peekup.lua
+++
@@ -1,5 +0,0 @@
--- Plugin para mejorar el uso del registro para copiar y pegar usa ""
--- https://github.com/gennaro-tedesco/nvim-peekup?tab=readme-ov-file
-return {
-  "gennaro-tedesco/nvim-peekup",
-}
