8104e02b1ecceb013853b6c06ee982e5fc94f7e0
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-01-16 20:42:52 -0600

    Fix(leap):Change Leap for Flash
    Se cambio el plugin leap por flash que ademas de agregar saltos mejora
    las busquedas.

--- lua/plugins/leap.lua
+++
@@ -1,7 +0,0 @@
--- Saltar a partes especificas facilmente https://github.com/ggandor/leap.nvim
-return {
-  'ggandor/leap.nvim',
-  config = function()
-    require('leap').add_default_mappings()
-  end,
-}
