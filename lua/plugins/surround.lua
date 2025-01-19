ef691f0377fe68d1aef947ee540abec14d3188e3
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-01-18 18:42:31 -0600

    feat(scrolling):Add pluging
    Se agrego un pluging que mejora el scrolling

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,10 +1,9 @@
 --Cambio facil de llaves y comillas https://github.com/kylechui/nvim-surround
 return {
-  'kylechui/nvim-surround',
-  version = "*",
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-    })
-  end
+	"kylechui/nvim-surround",
+	version = "*",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({})
+	end,
 }
