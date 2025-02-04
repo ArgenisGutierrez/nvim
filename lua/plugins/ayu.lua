62a8922c45b33f99b303af9143a6021f92691971
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-04 11:53:41 -0600

    feat:Colorscheme change
    se cambio del colorscheme ayu al de kanagawa wave

--- lua/plugins/ayu.lua
+++ lua/plugins/ayu.lua
@@ -1,19 +1,19 @@
 -- Colorscheme https://github.com/Shatur/neovim-ayu
 return {
-  'Shatur/neovim-ayu',
+  "Shatur/neovim-ayu",
   name = "ayu",
   lazy = false,
   priority = 1000,
   config = function()
-    require('ayu').setup({
+    require("ayu").setup({
       mirage = false,
       terminal = true,
       overrides = {
-        LineNr = { fg = '#3fa6ff' },
-        Comment = { fg = '#55bcc8' },
-        Normal = { fg = '#ecefef' },
-      }
+        LineNr = { fg = "#3fa6ff" },
+        Comment = { fg = "#55bcc8" },
+        Normal = { fg = "#ecefef" },
+      },
     })
-    vim.cmd([[colorscheme ayu-dark]])
-  end
+    -- vim.cmd([[colorscheme ayu-dark]])
+  end,
 }
