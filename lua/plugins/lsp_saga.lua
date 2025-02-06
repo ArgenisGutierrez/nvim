bc62badaea6bb8f418f4b04c1362d74ef308c61e
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-06 15:15:35 -0600

    feat:Lsp-saga
    se arreglo el diagnostico de lsp-saga

--- lua/plugins/lsp_saga.lua
+++ lua/plugins/lsp_saga.lua
@@ -1,7 +1,6 @@
 --Mejora funciones de lsp y agrega otras mas como el winbar y el navbar https://nvimdev.github.io/lspsaga/
 return {
   "nvimdev/lspsaga.nvim",
-  -- commit = 'bb399b24c70ff1a6e57a7057f66225daf8de3811',
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
@@ -26,7 +25,7 @@ return {
       { "<leader>l", group = " LSP" },
       { "<leader>ls", "<cmd>Lspsaga finder<cr>", desc = "󰮗 Finder", mode = "n" },
       { "<leader>lr", "<cmd>Lspsaga rename<cr>", desc = "󰑕 Rename", mode = "n" },
-      { "<leader>le", "<cmd>Lspsaga show_buf_diagnostics<cr>", desc = "󰩂 Diagnostic", mode = "n" },
+      { "<leader>le", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "󰩂 Diagnostic", mode = "n" },
       { "<leader>lk", "<cmd>Lspsaga hover_doc<cr>", desc = " Definicion", mode = "n" },
       { "<leader>lg", "<cmd>Lspsaga goto_definition<cr>", desc = " Go to Defition", mode = "n" },
       { "<leader>lc", "<cmd>Lspsaga code_action<cr>", desc = " Code Action", mode = "n" },
