e191b9678c07008cd503ce35d0ed3dc172e1bba7
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-06 00:10:57 -0600

    chore: Return to basis
    se regreo toda la conf a lo basico

--- lua/plugins/lsp.lua
+++
@@ -1,6 +0,0 @@
---LSP Config https://github.com/neovim/nvim-lspconfig
-return {
-  "neovim/nvim-lspconfig",
-  dependencies = {},
-  Config = function() end,
-}
--- lua/plugins/surround.lua
+++
@@ -1,9 +0,0 @@
---Cambio facil de llaves y comillas https://github.com/kylechui/nvim-surround
-return {
-	"kylechui/nvim-surround",
-	version = "*",
-	event = "VeryLazy",
-	config = function()
-		require("nvim-surround").setup({})
-	end,
-}
