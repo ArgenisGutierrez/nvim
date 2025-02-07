3758ff1068c359cab9df314057d750748bf57613
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-06 19:10:35 -0600

    fix: treesitter
    se arreglo el error que surgia al eliminar bloques de codigo o
    deshaciendo varios camnios

--- lua/plugins/tree_sitter.lua
+++ lua/plugins/tree_sitter.lua
@@ -1,58 +1,30 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate", -- Actualiza automáticamente los parsers
+    build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects", -- Soporte para objetos de texto
-      "nvim-treesitter/playground",                  -- Depuración y visualización del árbol sintáctico
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      -- Comenta playground si no lo usas:
+      -- "nvim-treesitter/playground",
     },
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
+          "query"
         },
-        auto_install = true,        -- Instala automáticamente nuevos parsers
-        highlight = { enable = true, additional_vim_regex_highlighting = false },
-        indent = { enable = true }, -- Indentación basada en Treesitter
-        incremental_selection = {   -- Mejora la selección de bloques de código
+        auto_install = false,  -- Importante!
+        highlight = { 
           enable = true,
-          keymaps = {
-            init_selection = "<CR>",
-            node_incremental = "<CR>",
-            scope_incremental = "<S-CR>",
-            node_decremental = "<BS>",
-          },
+          additional_vim_regex_highlighting = false,
+          disable = { "markdown" }  -- Desactiva para markdown si es problemático
         },
-        textobjects = { -- Mejora la selección y navegación de texto
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-          },
-        },
+        indent = { enable = false },  -- Desactiva indentación experimental
       })
 
-      -- Configurar Treesitter como plegado por defecto
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.opt.foldenable = false -- Desactiva el plegado al abrir archivos
+      -- Elimina la configuración de folding o usa manual
+      vim.opt.foldmethod = "manual"
+      vim.opt.foldenable = false
     end,
   },
 }
