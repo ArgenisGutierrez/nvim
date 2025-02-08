b000b6a0ae4d9240c2427af636a68f77097961ca
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-02-08 00:22:29 -0600

    style:Git-sign and noice
    se cambiaron los estilos de ambos plugins

--- lua/plugins/git_sign.lua
+++ lua/plugins/git_sign.lua
@@ -1,18 +1,32 @@
 -- Senales de git en el line number https://github.com/lewis6991/gitsigns.nvim
 return {
 	"lewis6991/gitsigns.nvim",
-  event = "VeryLazy",
-	opts = {
-		signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-		numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
-		linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-		word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-		signs      = {
-			add = { text = '▎' },
-			change = { text = '▎' },
-			delete = { text = '➤' },
-			topdelete = { text = '➤' },
-			changedelete = { text = '▎' },
-		},
-	}
+	event = "VeryLazy",
+	config = function()
+		require("gitsigns").setup({
+			signcolumn = true,
+			numhl = false,
+			linehl = false,
+			word_diff = false,
+			signs = {
+				add = { text = "" },
+				change = { text = "" },
+				delete = { text = "" },
+				topdelete = { text = "󰠙" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "" },
+				change = { text = "" },
+				delete = { text = "" },
+				topdelete = { text = "󰠙" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+		})
+		vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "none" })
+		vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none" })
+		vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none" })
+	end,
 }
